3bf453e0318ea2ef4a6b9c2067dd75fd6fa57656
Igor Postnikov <igor.postnikov8@gmail.com>
2020-03-14 18:14:16 -0700

    added student class

--- ClassGoat.py
+++ ClassGoat.py
@@ -1,9 +1,9 @@
 class Goat:
-    age = 0
-    name = ""
+    age = 0         # class attributes
+    name = ""       # class it is object type
     weight = 0.0
 
-    def show(self):
+    def show(self): # self - mandatory, link for the object for what it was called
         print(self.name)
         print(self.age)
 
@@ -12,7 +12,22 @@ a.name = "Notka"
 a.age = 2
 a.age += 1
 b = Goat()
-b.name = "Zorka"
-b.age = 1
+#b.name = "Zorka"
+#b.age = 1
 a.show()
 b.show()
+
+
+#####
+
+class Student:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print(name, "+")
+        # new attributes create only here
+
+a = Student("Vasja", 17) # "Vasja", 17 - constructor parametrs
+a.tail = "Fusica"
+print(dir(a))
