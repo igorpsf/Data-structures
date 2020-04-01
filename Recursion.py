245c3f766c3a0e606e83db8a002a2bce02ba6094
Igor Postnikov <igor.postnikov8@gmail.com>
2020-03-31 20:47:09 -0700

    added recursion.py

---
+++ Recursion.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n < 2:
+        return 1
+    return n * factorial(n - 1)
+
+#def recursive_function(parameters):
+#    if base_case_condition(parameters):
+#        return base_case_value
+#    recursive_function(modified_parameters)
